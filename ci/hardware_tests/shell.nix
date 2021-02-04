0d288fabe2dc4fcc211cada984e1872e6a72638a
Martin Milata <martin@martinmilata.cz>
2021-02-04 16:00:55 +0100

    ci: uhubctl from my nixpkgs clone

--- ci/hardware_tests/shell.nix
+++ ci/hardware_tests/shell.nix
@@ -1,4 +1,7 @@
-with import ../nixpkgs.nix;
+with import (builtins.fetchTarball {
+  url = "https://github.com/mmilata/nixpkgs/archive/99fdcb8acd2b916b84f9bf6586df8a6b6e67a370.tar.gz";
+  sha256 = "08i0y5q3da8g4pwffx8vv4rb37xvfncs1x1bh3hc3abbrxfpnc39";
+}) { };
 
 stdenv.mkDerivation rec {
   name = "trezor-firmware-hardware-tests";
